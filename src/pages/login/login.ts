80da91a8e27c4f272c9569ff41f4b001927d0150
David Castilllo <castillorganization@gmail.com>
2017-02-13 18:16:44 -0500

    facebook validation no pasa al home

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -5,7 +5,11 @@ import { HomePage } from '../home/home';
 import { SignupComponent } from '../signup/signup';
 import { EmailComponent } from '../email/email';
 
-import {NativeStorage} from 'ionic-native'
+import {NativeStorage} from 'ionic-native';
+
+import { Facebook } from 'ionic-native';
+import { Platform } from 'ionic-angular';
+import {GooglePlus} from 'ionic-native';
 
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
@@ -24,7 +28,7 @@ export class LoginPage implements OnInit{
   emailComponent = EmailComponent
 
   error: any;
-  constructor(public af: AngularFire, private nav: NavController,) {
+  constructor(public af: AngularFire, private nav: NavController, private platform: Platform) {
 
       this.af.auth.subscribe(auth => { 
       if(auth) {
@@ -35,14 +39,20 @@ export class LoginPage implements OnInit{
   }
 
   loginFb() {
-    this.af.auth.login({
+      if (this.platform.is('cordova')) {
+      return Facebook.login(['email', 'public_profile']).then(res => {
+        const facebookCredential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
+        return firebase.auth().signInWithCredential(facebookCredential) ;
+         });
+    } else {
+      return  this.af.auth.login({
       provider: AuthProviders.Facebook,
       method: AuthMethods.Popup,
-    });
-
+    })
+/*
         NativeStorage.setItem('user', {
         email: this.user.email,
-      })
+      })*/
       
     .then(
         (success) => {
@@ -51,6 +61,7 @@ export class LoginPage implements OnInit{
         (err) => {
         this.error = err;
       })
+    }
   }
 
   loginGoogle() {
