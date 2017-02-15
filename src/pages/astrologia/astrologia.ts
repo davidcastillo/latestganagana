f08f6fe83735c922939b10bede8939bd789d5bda
David Castilllo <castillorganization@gmail.com>
2017-02-14 19:13:12 -0500

    estilizacion al login-astrologia-formulario kit de la suerte

--- src/pages/astrologia/astrologia.ts
+++ src/pages/astrologia/astrologia.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { Validators, FormBuilder } from '@angular/forms';
+
 import { AstroresultadosPage } from '../astroresultados/astroresultados';
 
 
@@ -10,11 +12,19 @@ import { AstroresultadosPage } from '../astroresultados/astroresultados';
 })
 export class AstrologiaPage {
   astroresultadosPage = AstroresultadosPage
+     todo = {}
 
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-
+  constructor(public navParams: NavParams, private formBuilder: FormBuilder, public navCtrl: NavController)  {
+  
+      this.todo = this.formBuilder.group({
+      name: ['', Validators.required],
+      date_of_birth: ['',Validators.required],
+      time: ['',Validators.required],
+      city: ['',Validators.required],
+    });
+}
 
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -56,8 +56,8 @@ export class LoginPage implements OnInit {
         this.loader.dismiss();
       });
       let prompt = this.alertCtrl.create({
-        title: 'Success',
-        subTitle: 'Your new Account was created!',
+        title: 'Felicidades',
+        subTitle: 'Tu cuenta fue creada exitosamente',
         buttons: ['OK']
       });
       prompt.present();
@@ -83,7 +83,7 @@ export class LoginPage implements OnInit {
 
   loginAlert(): void {
     let prompt = this.alertCtrl.create({
-      title: 'login',
+      title: 'Ingresa',
       message: "Digita un usuario y contraseña.",
       inputs: [
         {
@@ -101,12 +101,14 @@ export class LoginPage implements OnInit {
       buttons: [
         {
           text: 'Cancel',
+           cssClass: 'alertDanger',
           handler: data => {
             console.log('Cancel clicked');
           }
         },
         {
           text: 'Entrar',
+          cssClass: 'alertDanger',
           handler: data => {
             this.login(data);
           }
